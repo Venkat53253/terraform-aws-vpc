b04b52474e3fd57273a938fabd58a0644db26962
Venkat53253 <venkatratna532@gmail.com>
2025-08-09 16:50:49 +0530

    Fix: removed duplicate peering resources from vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,53 +1,123 @@
-resource "aws_vpc_peering_connection" "default" {
-  count = var.is_peering_required ? 1 : 0
-
-  peer_vpc_id   = data.aws_vpc.default.id # acceptor VPC which is default here
-  vpc_id        = aws_vpc.main.id # requestor
-
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
-
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
-
-  auto_accept = true
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
 
   tags = merge(
-    var.vpc_peering_tags,
+    var.vpc_tags,
     local.common_tags,
     {
-        Name = "${var.project}-${var.environment}-default"
+      Name = "${var.project}-${var.environment}-vpc"
     }
   )
 }
 
-resource "aws_route" "public_peering" {
-  count = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.igw_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-igw"
+    }
+  )
 }
 
-resource "aws_route" "private_peering" {
-  count = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-public-${count.index + 1}"
+    }
+  )
 }
 
-resource "aws_route" "database_peering" {
-  count = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route_table.database.id
-  destination_cidr_block    = data.aws_vpc.default.cidr_block
-  vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private-${count.index + 1}"
+    }
+  )
 }
 
-# we should add peering connection in default VPC main route table too
-resource "aws_route" "default_peering" {
-  count = var.is_peering_required ? 1 : 0
-  route_table_id            = data.aws_route_table.main.id
-  destination_cidr_block    = var.cidr_block
-  vpc_peering_connection_id  = aws_vpc_peering_connection.default[count.index].id
+resource "aws_subnet" "database" {
+  count             = length(var.database_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.database_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-database-${count.index + 1}"
+    }
+  )
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-public-rt"
+    }
+  )
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private-rt"
+    }
+  )
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-database-rt"
+    }
+  )
+}
+
+resource "aws_route_table_association" "database" {
+  count          = length(aws_subnet.database)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
 }
